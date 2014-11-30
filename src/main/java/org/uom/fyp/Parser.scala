6d72eb3690a0238e42ffea9cd435e52ca327090e
Daniel <desiradaniel2007@hotmail.com>
2014-11-30 16:53:14 +0100

    initial commit

---
+++ src/main/java/org/uom/fyp/Parser.scala
@@ -0,0 +1,8 @@
+package org.uom.fyp
+
+/**
+ * Created by Daniel on 11/30/2014.
+ */
+class Parser {
+
+}
---
+++ src/main/java/org/uom/fyp/RoadNetwork.scala
@@ -0,0 +1,9 @@
+package org.uom.fyp
+
+/**
+ * Created by Daniel on 11/30/2014.
+ */
+trait RoadNetwork {
+  def applyDiversion() : Any
+  def findAverageTime() : Double
+}
