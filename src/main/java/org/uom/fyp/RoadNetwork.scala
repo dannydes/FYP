999cf4486123686f01a9f89c8556012896b51760
Daniel <desiradaniel2007@hotmail.com>
2015-02-10 13:35:33 +0100

    change findAverageTime() signature

--- src/main/java/org/uom/fyp/RoadNetwork.scala
+++ src/main/java/org/uom/fyp/RoadNetwork.scala
@@ -1,9 +1,11 @@
 package org.uom.fyp
 
+import org.jgrapht.util.VertexPair
+
 /**
  * Created by Daniel on 11/30/2014.
  */
 trait RoadNetwork {
   def applyDiversion() : Any
-  def findAverageTime() : Double
+  def findAverageTime(nodes : VertexPair) : Double
 }
