80dcaa4c0ef948f569eb29fe0203d8afce6e8d8e
Daniel <desiradaniel2007@hotmail.com>
2015-02-10 22:58:15 +0100

    Lane/Vehicle class structuring

---
+++ src/main/java/org/uom/fyp/Lane.scala
@@ -0,0 +1,10 @@
+package org.uom.fyp
+
+import scala.collection.mutable.Queue
+
+/**
+ * Created by Desira Daniel on 2/10/2015.
+ */
+class Lane extends Queue[Vehicle] {
+
+}
--- src/main/java/org/uom/fyp/RoadNetwork.scala
+++ src/main/java/org/uom/fyp/RoadNetwork.scala
@@ -6,6 +6,7 @@ import org.jgrapht.util.VertexPair
  * Created by Daniel on 11/30/2014.
  */
 trait RoadNetwork {
+
   def applyDiversion() : Any
   def findAverageTime(nodes : VertexPair[Node]) : Double
 
