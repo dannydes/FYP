aafd4f6fcd9f7260ef63f9c0950a9892994f28fd
Daniel <desiradaniel2007@hotmail.com>
2015-02-12 14:35:44 +0100

    added speed property for vehicle

--- src/main/java/org/uom/fyp/Vehicle.scala
+++ src/main/java/org/uom/fyp/Vehicle.scala
@@ -4,6 +4,11 @@ package org.uom.fyp
  * Created by Desira Daniel on 2/10/2015.
  */
 class Vehicle(l: Double, w: Double) {
+  var s: Int = 0
+
   def len: Double = l
   def width: Double = w
+
+  def speed = s
+  def speed_(s: Int): Unit = {}
 }
