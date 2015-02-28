89ea9f199eb112179a3538a9dc8c5668ab445fa8
Daniel <desiradaniel2007@hotmail.com>
2015-03-01 00:30:34 +0100

    implementation for two way lanes commented out (makes no sense anyway)

--- src/main/java/org/uom/fyp/TwoWayLane.scala
+++ src/main/java/org/uom/fyp/TwoWayLane.scala
@@ -4,12 +4,12 @@ package org.uom.fyp
  * Created by Desira Daniel on 2/10/2015.
  */
 class TwoWayLane(l: Double, w: Double) extends Lane(l, w) {
-  var lSide: Boolean = true
+  /**var lSide: Boolean = true
 
   def leftSide = lSide
 
   def changeSide(): Unit = {
     lSide = ! lSide
     super.reverse
-  }
+  }**/
 }
