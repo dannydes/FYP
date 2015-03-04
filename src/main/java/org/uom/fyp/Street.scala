adfeb8aab73b2cfced0913661fb9d410efd4fc1d
Daniel <desiradaniel2007@hotmail.com>
2015-03-04 10:27:57 +0100

    defined Street class

---
+++ src/main/java/org/uom/fyp/Street.scala
@@ -0,0 +1,9 @@
+package org.uom.fyp
+
+/**
+ * Created by Daniel on 3/4/2015.
+ */
+class Street(streetName: String, streetType: StreetType) {
+  def name = streetName
+  def sType = streetType
+}
---
+++ src/main/java/org/uom/fyp/StreetType.java
@@ -0,0 +1,8 @@
+package org.uom.fyp;
+
+/**
+ * Created by Daniel on 3/4/2015.
+ */
+public enum StreetType {
+    PRIMARY, SECONDARY
+}
