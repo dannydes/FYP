83a943fad631e6767f1275885ebf50300ab8c478
Daniel <desiradaniel2007@hotmail.com>
2015-03-04 10:37:36 +0100

    some RoadNetwork refactoring

--- src/main/java/org/uom/fyp/RoadNetwork.scala
+++ src/main/java/org/uom/fyp/RoadNetwork.scala
@@ -1,13 +1,11 @@
 package org.uom.fyp
 
-import java.util.Set
-
 import org.jgrapht.DirectedGraph
 import org.jgrapht.graph.DefaultDirectedGraph
 import org.jgrapht.util.VertexPair
 
 /**
- * Created by Daniel on 3/2/2015.
+ * RoadNetwork class
  */
 class RoadNetwork extends IRoadNetwork {
   override var graph: DirectedGraph[Node, Lane] = new DefaultDirectedGraph(classOf[Lane])
