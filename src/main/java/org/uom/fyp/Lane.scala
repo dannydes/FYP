8393c1eacc902181c6f4203ffe1f1088057b2468
Daniel <desiradaniel2007@hotmail.com>
2015-03-04 10:33:00 +0100

    Lane street property

--- src/main/java/org/uom/fyp/Lane.scala
+++ src/main/java/org/uom/fyp/Lane.scala
@@ -6,9 +6,10 @@ import org.jgrapht.graph.DefaultEdge
 /**
  * Lane class
  */
-class Lane(l: Double, w: Double) extends DefaultEdge {
+class Lane(l: Double, w: Double, s: Street) extends DefaultEdge {
   def lLen: Double = l
   def width: Double = w
+  def street = s
 
   def attachLane(properties: AnyRef): Lane = NetworkUtils.createLane(this.getTarget.asInstanceOf[Node])
   def block(streetName: String, network: DirectedGraph) = {}
