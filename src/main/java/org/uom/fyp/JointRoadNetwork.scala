8a18dca88f5add120a4121399002695022ad2965
Daniel <desiradaniel2007@hotmail.com>
2015-03-06 00:11:26 +0100

    renamed class

--- src/main/java/org/uom/fyp/JointRoadNetwork.scala
+++ src/main/java/org/uom/fyp/JointRoadNetwork.scala
@@ -5,4 +5,4 @@ import org.jgrapht.graph.{GraphUnion, DirectedGraphUnion}
 /**
  * Created by Daniel on 3/5/2015.
  */
-class JoinedRoadNetwork(n1: RoadNetwork, n2: RoadNetwork) extends RoadNetwork with Graph { }
+class JointRoadNetwork(n1: RoadNetwork, n2: RoadNetwork) extends RoadNetwork with Graph { }
