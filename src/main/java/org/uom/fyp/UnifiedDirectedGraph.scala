2569f4b2f313b4c312c391cb2411bed13b43118c
Daniel <desiradaniel2007@hotmail.com>
2015-03-06 23:18:32 +0100

    base of graph union algorithm

--- src/main/java/org/uom/fyp/UnifiedDirectedGraph.scala
+++ src/main/java/org/uom/fyp/UnifiedDirectedGraph.scala
@@ -6,5 +6,15 @@ import org.jgrapht.graph.DefaultDirectedGraph
  * Created by Daniel on 3/6/2015.
  */
 class UnifiedDirectedGraph(g1: DefaultDirectedGraph[Node, Lane], g2: DefaultDirectedGraph[Node, Lane]) extends DefaultDirectedGraph[Node, Lane](classOf[Lane]) {
+  val g1EdgeIterator = g1.edgeSet.iterator
+  val g2EdgeIterator = g2.edgeSet.iterator
+  while (g1EdgeIterator.hasNext || g2EdgeIterator.hasNext) {
 
+  }
+
+  val g1VertexIterator = g1.vertexSet.iterator
+  val g2VertexIterator = g2.vertexSet.iterator
+  while (g1VertexIterator.hasNext || g2VertexIterator.hasNext) {
+
+  }
 }
