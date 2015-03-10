f9094a8a011ecb80654c5c3942710450aa88e851
Daniel <desiradaniel2007@hotmail.com>
2015-03-10 01:19:26 +0100

    some more ScalaDoc

--- src/main/java/org/uom/fyp/engine/Vehicle.scala
+++ src/main/java/org/uom/fyp/engine/Vehicle.scala
@@ -6,13 +6,33 @@ package org.uom.fyp.engine
  * @param w Width of vehicle.
  */
 class Vehicle(l: Double, w: Double) {
+
+  /**
+   * Stores speed.
+   */
   var s: Int = 0
 
+  /**
+   * Returns length of vehicle.
+   */
   def len: Double = l
+
+  /**
+   * Returns width of vehicle.
+   */
   def width: Double = w
 
+  /**
+   * Returns speed of vehicle.
+   */
   def speed = s
+
+  /**
+   * Sets speed of vehicle.
+   * @param s Speed of vehicle.
+   */
   def speed_=(s: Int) {
     this.s = s
   }
+  
 }
