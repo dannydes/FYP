9e1709de8c2d0f282711318e32500ce385abe081
Daniel <desiradaniel2007@hotmail.com>
2015-03-15 21:52:24 +0100

    ScalaDoc

--- src/main/java/org/uom/fyp/engine/UnifiedDirectedGraph.scala
+++ src/main/java/org/uom/fyp/engine/UnifiedDirectedGraph.scala
@@ -5,16 +5,22 @@ import java.util
 import org.jgrapht.graph.DefaultDirectedGraph
 
 /**
- * Created by Daniel on 3/6/2015.
+ * Replaces JGraphT's DriectedGraphUnion class with a different implementation.
  */
 class UnifiedDirectedGraph(g1: DefaultDirectedGraph[Node, Lane], g2: DefaultDirectedGraph[Node, Lane]) extends DefaultDirectedGraph[Node, Lane](classOf[Lane]) {
   jSetUnion(g1.edgeSet, g2.edgeSet)
   jSetUnion(g2.vertexSet, g2.vertexSet)
 
+  /**
+   * Returns the union of two sets, s1 and s2.
+   * @param s1 Set 1.
+   * @param s2 Set 2.
+   */
   private def jSetUnion(s1: util.Collection[_], s2: util.Collection[_]): util.HashSet[_] = {
     val res = new util.HashSet[Any]
     res.addAll(s1)
     res.addAll(s2)
     res
   }
+
 }
