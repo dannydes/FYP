33e09b982b3244667259673cbd4e5f4d8769124e
Daniel <desiradaniel2007@hotmail.com>
2015-03-31 00:21:00 +0200

    create class

---
+++ src/test/GraphTest.scala
@@ -0,0 +1,16 @@
+import org.scalatest._
+
+/**
+ * Contains tests the graph-based functionality of the engine behind the
+ * language.
+ */
+class GraphTest {//extends FlatSpec with Matchers {
+
+  /**
+   * Tests the blocking a lane belonging to a street with a given name.
+   */
+//  def blockLane(): Unit = {
+
+//  }
+
+}
