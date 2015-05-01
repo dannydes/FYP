86a5a196100271fbd93214f456ce5d4abe3b7844
Daniel <desiradaniel2007@hotmail.com>
2015-05-02 00:18:14 +0200

    imported Parser Combinators

--- src/main/java/org/uom/fyp/dslfrontend/Parser.scala
+++ src/main/java/org/uom/fyp/dslfrontend/Parser.scala
@@ -1,9 +1,11 @@
 package org.uom.fyp.dslfrontend
 
+import scala.util.parsing.combinator.JavaTokenParsers
+
 /**
  * Forms a parse tree from an array of tokens, depending on their type and
  * content.
  */
-class Parser {
+class Parser extends JavaTokenParsers {
 
 }
