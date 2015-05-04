ae6ec1b8b0e2e694760563393a2be392e050deb5
Daniel <desiradaniel2007@hotmail.com>
2015-05-04 11:36:06 +0200

    converted to object/added launcher method

--- src/main/java/org/uom/fyp/dslfrontend/Parser.scala
+++ src/main/java/org/uom/fyp/dslfrontend/Parser.scala
@@ -3,12 +3,13 @@ package org.uom.fyp.dslfrontend
 import org.uom.fyp.engine.{RoadNetwork, RoadNetworkUnion}
 
 import scala.util.parsing.combinator.JavaTokenParsers
+import scala.io.Source
 
 /**
  * Forms a parse tree from an array of tokens, depending on their type and
  * content.
  */
-class Parser extends JavaTokenParsers {
+object Parser extends JavaTokenParsers {
 
   def statement = constructNetwork.+ ~ joinNetworks.? ~ given ~ runSimulation
 
@@ -29,5 +30,13 @@ class Parser extends JavaTokenParsers {
   def attachRoad = "attach" ~ ("primary" | "secondary") ~ "road" ~ stringLiteral ~ "with" ~ "length" ~ floatingPointNumber
 
   def blockRoad = "block" ~ stringLiteral
+
+  def parse(filename: String) = {
+    parseAll(statement, Source.fromFile(filename).mkString) match {
+      case Success(query, _) => println("Yeah!")
+      case Failure(msg, _) => println(msg)
+      case Error(msg, _) => println(msg)
+    }
+  }
 
 }
