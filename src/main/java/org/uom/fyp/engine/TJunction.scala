9b390837631164ad3e01c30bb537c58a8d6a6f3e
Daniel <desiradaniel2007@hotmail.com>
2015-07-20 14:11:31 +0200

    changed property name

--- src/main/java/org/uom/fyp/engine/TJunction.scala
+++ src/main/java/org/uom/fyp/engine/TJunction.scala
@@ -3,8 +3,8 @@ package org.uom.fyp.engine
 /**
  * Created by Daniel on 7/14/2015.
  */
-case class TJunction(priority: Edge, c1: Edge, c2: Edge) extends Node {
-  val tPriority = priority
+case class TJunction(p: Edge, c1: Edge, c2: Edge) extends Node {
+  val priority = p
   val converging1 = c1
   val converging2 = c2
 }
