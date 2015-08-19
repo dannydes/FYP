c7d0276feab5b2720231ca535a9db618e17364e0
Daniel <desiradaniel2007@hotmail.com>
2015-08-19 22:53:31 +0200

    some work with errors :'(

--- src/main/java/org/uom/fyp/engine/Edge.scala
+++ src/main/java/org/uom/fyp/engine/Edge.scala
@@ -53,10 +53,6 @@ class Edge extends DefaultEdge {
 
   private var edgeType: RoadStructure.EnumVal = RoadStructure.Default
 
-  private var pdqNodeNo: Int = _
-
-  var residenceTime: Double = _
-
   private var minSim: Double = _
 
   private var sLanes: Int = _
@@ -76,8 +72,17 @@ class Edge extends DefaultEdge {
     this.sName = sName
   }
 
+  /**
+   * Returns a number depending on the edge's occurrence on the street
+   * (order considered in ascending order of intersection point).
+   */
   def streetEdgeNo = sEdgeNo
 
+  /**
+   * Sets a number depending on the edge's occurrence on the street
+   * (order considered in ascending order of intersection point)
+   * @param sEdgeNo The number.
+   */
   def streetEdgeNo_(sEdgeNo: Int) = {
     this.sEdgeNo = sEdgeNo
   }
@@ -129,14 +134,14 @@ class Edge extends DefaultEdge {
 
   /**
    * Returns a reference to the <b>Street</b> object representing the physical
-   * lane that intersects with the street on which the edge is found. Used in
+   * street that intersects with the street on which the edge is found. Used in
    * the graph building process.
    */
   def streetAtTarget = sat
 
   /**
    * Sets a reference to the <b>Street</b> object representing the physical
-   * lane that intersects with the street on which the edge is found.
+   * street that intersects with the street on which the edge is found.
    * @param sat <b>Street</b> object intersecting with the street from which the
    *            edge is taken to create the edge.
    */
@@ -144,8 +149,17 @@ class Edge extends DefaultEdge {
     this.sat = sat
   }
 
+  /**
+   * Returns a reference to the street that intersects the street on which this edge is
+   * found. Used in the graph building process.
+   */
   def streetAtSource = sas
 
+  /**
+   * Sets a reference to the street that intersects the street on which this edge is
+   * found. To be called if and only if intersection point is 0.
+   * @param sas Street intersecting street on which this edge is found.
+   */
   def streetAtSource_(sas: Street) = {
     this.sas = sas
   }
@@ -164,52 +178,83 @@ class Edge extends DefaultEdge {
   }
 
   /**
-   * Returns the street's arrival rate.
+   * Returns the edge's arrival rate.
    */
   def arrivalRate = lambda
 
   /**
-   * Sets the street's arrival rate.
-   * @param lambda The street's arrival rate.
+   * Sets the edges's arrival rate.
+   * @param lambda The edge's arrival rate.
    */
   def arrivalRate_(lambda: Double) = {
     this.lambda = lambda
   }
 
   /**
-   * Returns the street's departure rate.
+   * Returns the edge's departure rate.
    */
   def departureRate = lambda / (1 - noOfVehicles)
 
+  /**
+   * Returns the edge's servicing time.
+   */
   def serviceTime = vehicles / lambda * (1 + vehicles)
 
+  /**
+   * Returns the density within the edge.
+   */
   def density = vehicles / len
 
+  /**
+   * Returns the time in minutes for the simulation to run.
+   */
   def minutesForSimulation = minSim
 
+  /**
+   * Sets the time in minutes for the simulation to run at edge-level.
+   * @param minSim The time in minutes.
+   */
   def minutesForSimulation_(minSim: Double) = {
     this.minSim = minSim
   }
 
+  /**
+   * Returns the flow within the edge.
+   */
   def flow = vehicles / minSim
 
-  var speed = flow / density
+  /**
+   * Returns the traffic speed within the edge.
+   */
+  def speed = flow / density
 
   /**
-   * Returns the time taken for one job (vehicle) spends in a road segment (residence time).
+   * Returns the time taken for one job (vehicle) spends in a road segment.
    */
-  def time: Double = {
-    1 / (departureRate - lambda)
-  }
+  def residenceTime = 1 / (departureRate - lambda)
 
+  /**
+   * Returns the type of the edge's target node to-be.
+   */
   def edgeT = edgeType
 
+  /**
+   * Sets the type of the edge's target node to-be.
+   * @param edgeType The type of the edge's target node to-be.
+   */
   def edgeT_(edgeType: RoadStructure.EnumVal) = {
     this.edgeType = edgeType
   }
 
+  /**
+   * Returns the number of lanes belonging to the street containing this edge.
+   */
   def streetLanes = sLanes
 
+  /**
+   * Sets the number of lanes belonging to the street containing this edge.
+   * @param sLanes Number of lanes.
+   */
   def streetLanes_(sLanes: Int) = {
     this.sLanes = sLanes
   }
@@ -221,38 +266,33 @@ class Edge extends DefaultEdge {
    */
   def simulate(pdq: PDQ) = {
     var l: Int = 0
-    pdqNodeNo = pdq.noNodes
     for (l <- 0 until sLanes) {
-
-      pdq.CreateOpen(toString + "l" + l + "w", lambda)
-      pdq.CreateNode(toString + "l" + l, defs.CEN, defs.FCFS)
-      //pdq.SetDemand(toString, toString + "load", serviceTime)
-      pdq.SetVisits(toString + "l" + l, toString + "l" + l + "w", vehicles / sLanes, serviceTime)
+      val node = toString + "l" + l
+      val workload = node + "w"
+      pdq.CreateClosed(workload, Job.TERM, vehicles / l, )
+      pdq.CreateNode(node, defs.CEN, defs.FCFS)
+      pdq.SetDemand(node, workload, serviceTime)
       pdq.SetTUnit("Minutes")
       pdq.SetWUnit("Vehicles")
-
     }
   }
 
   /**
-   * Returns the index of the corresponding node in PDQ's <b>node</b> array.
-   */
-  def pdqNodeIndex = pdqNodeNo
-
-  /**
    * Returns the edge's source.
    */
-  override def getSource = {
-    super.getSource.asInstanceOf[Node]
-  }
+  override def getSource = super.getSource.asInstanceOf[Node]
 
   /**
    * Returns the edge's target.
    */
-  override def getTarget = {
-    super.getTarget.asInstanceOf[Node]
-  }
+  override def getTarget = super.getTarget.asInstanceOf[Node]
 
+  /**
+   * Returns a string representation of the edge, consisting of the name of the
+   * street to which the edge belongs, a number depending on the edge's occurrence
+   * on the street (order considered in ascending order of intersection point) and
+   * the edge's length.
+   */
   override def toString = sName + " " + sEdgeNo + " " + len
 
 }
